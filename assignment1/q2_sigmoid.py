29cc00a2959203c63647c76f0593d4a6da7ca3fc
muchen.pl <muchen.pl@alibaba-inc.com>
2017-11-07 19:24:36 +0800

    q2_sigmoid finish

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -4,40 +4,12 @@ import numpy as np
 
 
 def sigmoid(x):
-    """
-    Compute the sigmoid function for the input here.
-
-    Arguments:
-    x -- A scalar or numpy array.
-
-    Return:
-    s -- sigmoid(x)
-    """
-
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-
+    s = 1 / (1 + np.exp(-x))
     return s
 
 
 def sigmoid_grad(s):
-    """
-    Compute the gradient for the sigmoid function here. Note that
-    for this implementation, the input s should be the sigmoid
-    function value of your original input x.
-
-    Arguments:
-    s -- A scalar or numpy array.
-
-    Return:
-    ds -- Your computed gradient.
-    """
-
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-
+    ds = s * ( 1 - s)
     return ds
 
 
@@ -78,4 +50,4 @@ def test_sigmoid():
 
 if __name__ == "__main__":
     test_sigmoid_basic();
-    test_sigmoid()
+    #test_sigmoid()
